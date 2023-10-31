8ae4ba1246170456fdc9f8c138bd67c4beb1da05
Bo <bo.hui@jhu.edu>
2023-10-31 13:49:04 -0400

    Add evaluate

--- evaluate.py
+++ evaluate.py
@@ -7,10 +7,10 @@ with open('results/llama_SIQA_llama_16.csv', mode ='r') as file:
                 
     for lines in csvFile:
         trigger = [*lines][1]
-        text = lines[trigger].split('context:')
-        lines[trigger] = 'context:' + text[1] if len(text) > 1 else text[0]
-        text =  lines[trigger].split(trigger[:10])
-        lines[trigger] = text[0]
+        # text = lines[trigger].split('context:')
+        # lines[trigger] = 'context:' + text[1] if len(text) > 1 else text[0]
+        # text =  lines[trigger].split(trigger[:10])
+        # lines[trigger] = text[0]
         results.append(lines)
 attack = Sampler()
 attack.evaluate(results, level='em')
